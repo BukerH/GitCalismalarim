c04a49ca3c0096263ee473416c9ca03732b801ce
BukerH <h.buker@outlook.com>
2022-03-17 18:04:02 +0000

    All Project Files Uploaded

---
+++ src/test/java/day1/HelGit.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class HelGit {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+        System.out.println("2.satir calisma");
+    }
+}
+
